140023caebfa96f4b9a213249a9dcac0e47c5ca7
Fajar Muhammad Fathirochman <fajarmuhf@gmail.com>
2022-10-20 21:28:37 +0700

    init project

---
+++ Sewa/Sewa/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Xamarin.Forms;
+
+namespace Sewa
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly IGoogleManager _googleManager;
+        GoogleUser GoogleUser = new GoogleUser();
+        public bool IsLogedIn { get; set; }
+
+        public MainPage()
+        {
+            _googleManager = DependencyService.Get<IGoogleManager>();
+            CheckUserLoggedIn();
+            InitializeComponent();
+        }
+
+        private void CheckUserLoggedIn()
+        {
+            //_googleManager.Login(OnLoginComplete);
+        }
+
+        private void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            _googleManager.Login(OnLoginComplete);
+        }
+        private void OnLoginComplete(GoogleUser googleUser, string message)
+        {
+            if (googleUser != null)
+            {
+                GoogleUser = googleUser;
+                txtName.Text = GoogleUser.Name;
+                txtEmail.Text = GoogleUser.Email;
+                //imgProfile.Source = GoogleUser.Picture;
+                IsLogedIn = true;
+            }
+            else
+            {
+                DisplayAlert("Message", message, "Ok");
+            }
+        }
+        private void GoogleLogout()
+        {
+            _googleManager.Logout();
+            IsLogedIn = false;
+        }
+        private void btnLogout_Clicked(object sender, EventArgs e)
+        {
+            _googleManager.Logout();
+
+            txtName.Text = "Name :";
+            txtEmail.Text = "Email: ";
+            //imgProfile.Source = "";
+        }
+    }
+}
